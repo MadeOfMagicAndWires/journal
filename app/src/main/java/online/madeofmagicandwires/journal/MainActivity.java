df6f570ba8eec4d1baa1ed0d6e5f3900122b2386
Joost Bremmer <contact@madeofmagicandwires.online>
2018-12-20 20:56:52 +0100

    Add all activities

--- app/src/main/java/online/madeofmagicandwires/journal/MainActivity.java
+++ app/src/main/java/online/madeofmagicandwires/journal/MainActivity.java
@@ -5,15 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,27 +29,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initJournalList();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    /**
+     * Initiates and links ListView to adapter and entries.
+     */
+    public void initJournalList(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        ListView list = findViewById(R.id.entriesList);
+
+        View emptyState = inflater.inflate(R.layout.entries_list_empty_state, list, false);
+        list.setEmptyView(emptyState);
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
