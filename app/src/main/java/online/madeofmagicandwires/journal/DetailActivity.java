260704ee84f825a773bc490589105c19c52fa03a
Joost Bremmer <contact@madeofmagicandwires.online>
2018-12-29 21:35:48 +0100

    Add bare DetailActivity to show Journal entries

--- app/src/main/java/online/madeofmagicandwires/journal/DetailActivity.java
+++ app/src/main/java/online/madeofmagicandwires/journal/DetailActivity.java
@@ -3,11 +3,16 @@ package online.madeofmagicandwires.journal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends JournalActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+    }
+
+    @Override
+    public void updateState() {
+        // TODO: implement
     }
 }
